726a4dd4393d1447f2d0a0c3bed4389509d5934a
czecha02 <aik.czech@tciconsult.eu>
2026-01-08 12:20:47 +0100

    Fix navigation overlay with Z-9999 and apply 75% zoom

--- src/components/LessonNavigation.tsx
+++ src/components/LessonNavigation.tsx
@@ -23,7 +23,7 @@ export default function LessonNavigation({ currentLessonId }: LessonNavigationPr
     const nextLesson = currentLessonId < 7 ? lessonMap[currentLessonId + 1] : null
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 bg-white border-t-2 border-gray-300 z-[200]">
+        <div className="fixed bottom-0 left-0 right-0 bg-white border-t-2 border-gray-300 z-[9999]">
             <div className="max-w-4xl mx-auto px-4 py-4 flex items-center justify-between gap-4">
                 {previousLesson ? (
                     <Link
